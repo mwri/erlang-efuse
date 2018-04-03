9513edfb6321fac166c35171e7bf75d7cd5db840
Michael Wright <mjw@methodanalysis.com>
2018-04-03 23:51:23 +0100

    change license to MIT

--- src/efuse_app.erl
+++ src/efuse_app.erl
@@ -1,18 +1,7 @@
 %% This file is part of the Erlang FUSE (Filesystem in Userspace)
 %% interface called 'efuse'.
 %% 
-%% 'efuse' is free software: you can redistribute it and/or modify
-%% it under the terms of the GNU General Public License as published by
-%% the Free Software Foundation, either version 3 of the License, or
-%% (at your option) any later version.
-%% 
-%% This program is distributed in the hope that it will be useful,
-%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%% GNU General Public License for more details.
-%% 
-%% You should have received a copy of the GNU General Public License
-%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%% 'efuse' is free software, licensed under the MIT license.
 %% 
 %% @author Michael Wright <mjw@methodanalysis.com>
 %% @copyright 2015 Michael Wright <mjw@methodanalysis.com>
@@ -33,13 +22,13 @@
 
 start(_StartType, _StartArgs) ->
     efuse_sup:start_link()
-	.
+    .
 
 
 %% @private
 
 stop(_State) ->
     ok
-	.
+    .
 
 
